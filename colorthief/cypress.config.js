4e1d5c07cfcb717216c154a0586e8ae9cb758d0a
Gilles Piou <gilles.piou@gmail.com>
2023-01-22 22:14:44 +0800

    colorthief: Update dependencies

--- colorthief/cypress.config.js
+++ colorthief/cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
     },
+    testIsolation: false,
   },
   video: false,
 });
